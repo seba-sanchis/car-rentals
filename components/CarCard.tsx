ec9a06f2df77c4dd6aeb06c90b8134018bd36ec0
Sebastian Sanchis <seba.sanchis@gmail.com>
2023-06-11 20:30:38 -0300

    car images

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 import { CarProps } from "@/types";
 import CustomButton from "./CustomButton";
-import { calculateCarRent } from "@/utils";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 import CarDetails from "./CarDetails";
 
 interface CardCarProps {
@@ -35,7 +35,7 @@ const CarCard = ({ car }: CardCarProps) => {
 
       <div className="relative w-full h-40 my-3 object-contain">
         <Image
-          src="/hero.png"
+          src={generateCarImageUrl(car)}
           alt="car model"
           fill
           priority
